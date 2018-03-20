6a69c7e7eb347211dd1d235e8bfd9d1b69611f34
semegoita <34927635+semegoita@users.noreply.github.com>
2018-03-20 01:17:02 -0700

    PSuedo code added

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+
+var randomResult;
+var lost;
+var win;
+
+
+var random = Math.floor(Math.random()*101 +19);
+// psuedo code
+// a game with 4 crystalls
+// every crystal need to have a random number between 1-12
+//a new random number should be generated every time we win or lose
+// when clicking on any crystal it should add with previous result
+// untill it equals the total score
+// if it is not equal we start over
+// it it is equall we increment  win
+//
+
+
+ });
